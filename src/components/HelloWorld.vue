8f3b29088a293439a9bbe7dcba459774f77b88c4
sujk912 <aa991122aa@gmail.com>
2022-03-02 19:10:19 +0900

    first assign finished

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,67 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+  <h3> &lt; 강의 목록 > </h3>
+  <ul>
+      <li v-for="(item, idx) in lectures" :key="{idx}">
+        <div class="showBox">
+          <div class="el"><span class="tit">제목: </span>{{item.name}}</div>
+          <div class="el"><span class="tit">가격: </span> {{item.price}}</div>
+          <div class="el"><span class="tit">수량: </span> {{item.quantity}}</div>
+        </div>
+        <hr>
+        <div class="editBox">
+          <div class="el"><span class="tit">가격: </span> <input type="text" v-model="item.price"></div>
+          <div class="el"><span class="tit">수량: </span> <input type="text" v-model="item.quantity"></div>
+        </div>
+
+      </li>
     </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <table>
+    <tr>
+      <th>총 수량 :</th>
+      <td><span>{{quantity_sum}}</span> 개</td>
+    </tr>
+    <tr>
+      <th>총 가격 :</th>
+      <td><span>{{price_sum}}</span> 원</td>
+    </tr>
+    <tr>
+      <th>세후 가격 :</th>
+      <td><span>{{tax_result}}</span> 원</td>
+    </tr>
+  </table>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
+  data() {
+    return {
+      lectures : [
+          {name : "vue 기초", price : 1000, quantity : 5} ,
+          {name : "react 기초", price : 1500, quantity : 4} ,
+          {name : "angular 기초", price : 2000, quantity : 4} ,
+      ]
+    }
+  },
+  computed: {
+    price_sum() {
+      let sum = 0;
+      this.lectures.forEach(e => {
+        sum += e.price*e.quantity;
+      });
+      return sum;
+    },
+    quantity_sum(){
+      let sum=0;
+      this.lectures.forEach(e=>{
+        sum+=e.quantity;
+      })
+      return sum;
+    },
+    tax_result(){
+      return this.price_sum*1.1;
+    },
+  },
   props: {
     msg: String
   }
@@ -41,8 +70,12 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
 h3 {
   margin: 40px 0 0;
+  font-weight: bolder;
+  color: #42b983;
+  font-size: 40px;
 }
 ul {
   list-style-type: none;
@@ -51,8 +84,24 @@ ul {
 li {
   display: inline-block;
   margin: 0 10px;
+  background-color: whitesmoke;
+  border: #42b983 3px solid;
+  border-radius: 10%;
+  padding: 1rem 0.5rem 1rem 0.5rem;
+  text-align: start;
 }
-a {
+.tit{
+  font-weight: bold;
+}
+table {
+  text-align: left;
+  margin-left: auto;
+  margin-right: auto;
+}
+th {
   color: #42b983;
+}
+td{
+  text-align: right;
 }
 </style>
