f800292c64096a67641778da306b6eda53d3be8c
sujk912 <aa991122aa@gmail.com>
2022-03-13 16:26:54 +0900

    props emit이용해서 체크박스에 체크 들어오게 변경

---
+++ Sujeong/secondweek/src/components/HelloWorld.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+      <p>TODO - LIST</p>
+      <ul>
+        <li
+          v-for="item in li" 
+          :key="item.id" 
+        >
+          <todoItem 
+            :item="item" 
+            @checking-item="checkingItem"
+          ></todoItem>
+        </li>
+      </ul>
+      
+  </div>
+</template>
+
+<script>
+import TodoItem from "./TodoItem.vue"
+
+export default {
+  components: {TodoItem},
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+        li : [
+        {
+          id: 0,
+          content:"공부하자",
+          checked : false,
+        },
+        {
+          id: 1,
+          content:"테스트",
+          checked : false,
+        },
+        {
+          id: 2,
+          content:"222",
+          checked : false,
+        },
+      ]
+    }
+  },
+  methods: {
+    checkingItem(index) {
+      this.li[index].checked = !this.li[index].checked;
+      console.log(this.li);
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ul {
+  list-style-type: none;
+}
+li {
+  margin: 10px;
+  width: 100%;
+}
+
+</style>
---
+++ Sujeong/secondweek/src/components/TodoItem.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="wrapBox">
+        <p>
+            <span class="index">{{item.id}}</span>
+            <span class="content">{{item.content}}</span>
+            <input type="checkbox" :value="item.checked" v-on:click="onClickedBox"/>
+        </p>   
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ToDoElement',
+    props: {
+        item: {
+            type : Object,
+            default: null,
+        }
+    },
+    methods :{
+        onClickedBox(){
+            this.$emit("checking-item", this.item.id);
+        }
+    }
+}
+</script>
+
+<style>
+    .wrapBox{
+        background-color: beige;
+        border-radius: 30%;
+        
+    }
+</style>
